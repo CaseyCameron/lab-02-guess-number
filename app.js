b4f63574891418a426dcb7dbde98cdf34379cd45
Casey Cameron <mcasey.cameron@gmail.com>
2021-03-24 16:47:45 -0700

    ok it works at least

--- app.js
+++ app.js
@@ -1,17 +1,58 @@
-import { compareNumbers } from './utils.js';
+import { compareNumbers, resetGame, resetButtons } from './utils.js';
 
-const correctNumber = Math.floor(Math.random() * 101);
-let userGuess = document.getElementById('user-number-input');
-let displayGuessResult = document.getElementById('display-guess-result');
+let correctNumber = Math.floor(Math.random() * 101);
+const userGuess = document.getElementById('user-number-input');
+const displayGuessResult = document.getElementById('display-guess-result');
+const guessesRemainingCounter = document.getElementById('guesses-remaining');
+const showGameTally = document.getElementById('games-won-lost');
 const button = document.getElementById('submit-button');
+const resetButton = document.getElementById('reset-button');
+
+let guessesRemaining = 4;
+const gameTally = {
+    wins: 0,
+    losses: 0,
+};
 
 button.addEventListener('click', () => {
     console.log('Computer number = ' + correctNumber);
     console.log('user number = ' + userGuess.value);
+
     let guessResult = compareNumbers(userGuess.value, correctNumber);
     displayGuessResult.textContent = guessResult;
+    guessesRemaining--;
+    guessesRemainingCounter.textContent = `You have ${guessesRemaining} tries left.`;
+    console.log(guessesRemainingCounter);
+
+    if (guessesRemaining > 0 && guessResult === 'You win!'){
+        gameTally.wins++;
+        showGameTally.textContent = `Wins: ${gameTally.wins} / Losses: ${gameTally.losses}`;
+        const playAgain = confirm('You win! Play again?');
+        if (!playAgain) return;
+
+        // else continue playing
+
+        correctNumber = Math.floor(Math.random(1) * 101);
+        guessesRemaining = 4;
+        guessesRemainingCounter.textContent = `You have ${guessesRemaining} tries left.`;
+        displayGuessResult.textContent = 'Your guess results appear here.';
+
+    } 
+    if (guessesRemaining === 0){
+        gameTally.losses++;
+        showGameTally.textContent = `Wins: ${gameTally.wins} / Losses: ${gameTally.losses}`;
+        const playAgain = confirm('You lose! Play again?');
+        if (!playAgain) return;
+
+        // else continue playing
+
+        correctNumber = Math.floor(Math.random(1) * 101);
+        guessesRemaining = 4;
+        guessesRemainingCounter.textContent = `You have ${guessesRemaining} tries left.`;
+        displayGuessResult.textContent = 'Your guess results appear here.';
+    }
 });
 
-
-
-// set event listeners to update state and DOM
+resetButton.addEventListener('click', () => {
+    resetButtons(resetButton, button);
+});
--- utils.js
+++ utils.js
@@ -3,3 +3,14 @@ export function compareNumbers(userGuess, correctNumber){
     if (userGuess < correctNumber) return 'Too low';
     return 'You win!';
 }
+
+export function resetGame(showGameTally, gameTally, displayGuessResult, guessesRemainingCounter){
+    showGameTally.textContent = `Wins: ${gameTally.wins} / Losses: ${gameTally.losses}`;
+    displayGuessResult.textContent = 'Start a new game!';
+    guessesRemainingCounter.textContent = 'Guesses Remaining';
+}
+
+export function resetButtons(buttonOne, buttonTwo){
+    buttonOne.style.display = 'none';
+    buttonTwo.style.display = 'block';
+}
